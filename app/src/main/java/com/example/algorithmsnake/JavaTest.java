13f662553d643dd8e85d0b4aef373a9b2242e448
sebahag <sebahags@proton.me>
2025-03-27 18:23:13 +0200

    initial commit of migration to android

---
+++ app/src/main/java/com/example/algorithmsnake/JavaTest.java
@@ -0,0 +1,7 @@
+package com.example.algorithmsnake;
+
+public class JavaTest {
+    public static void printMessage() {
+        System.out.println("Java is working inside Kotlin project!");
+    }
+}
